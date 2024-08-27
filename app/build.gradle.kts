e444f053db0c24c8447c8e0d90b8fff2e0f42fd9
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-08-27 13:43:34 +0200

    spotless

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,18 @@
+plugins {
+    id("com.diffplug.spotless") version "6.25.0"
+    java
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.infinispan:infinispan-core:15.0.8.Final")
+
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    implementation(libs.guava)
 }
 
 java {
@@ -19,4 +23,15 @@ java {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+allprojects {
+    spotless {
+        java {
+            googleJavaFormat()
+            indentWithSpaces()
+            removeUnusedImports()
+            endWithNewline()
+        }
+    }
 }
